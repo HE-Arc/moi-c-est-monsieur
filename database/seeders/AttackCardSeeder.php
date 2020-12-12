0a7e6970488e2f8f51a6cdefd9aa87dda243fda3
He-Adrian <adrian.buzala@he-arc.ch>
2020-12-12 15:55:51 +0100

    added seeders

--- database/seeders/AttackCardSeeder.php
+++ database/seeders/AttackCardSeeder.php
@@ -15,8 +15,232 @@ class AttackCardSeeder extends Seeder
     public function run()
     {
         DB::table('attack_cards')->insert([
-            'attack' => "Tu n'est pas beau",
-            'category_id' => "1",
-        ]);
+            'id' => "1",
+        'attack' => "Les femmes ne sont pas encore dans ton pokédex ?",
+        'category_id' => "2",
+    ]);
+    
+    DB::table('attack_cards')->insert([
+            'id' => "2",
+        'attack' => "Tu sens le cul",
+        'category_id' => "1",
+    ]);
+    
+    DB::table('attack_cards')->insert([
+            'id' => "3",
+        'attack' => "Alors, ta mère a préparé mon goûter ce matin?",
+        'category_id' => "3",
+    ]);
+    
+    DB::table('attack_cards')->insert([
+            'id' => "4",
+        'attack' => "Face de rat",
+        'category_id' => "4",
+    ]);
+    
+    DB::table('attack_cards')->insert([
+            'id' => "5",
+        'attack' => "T'as la gueule remplie de pustules",
+        'category_id' => "4",
+    ]);
+    
+    DB::table('attack_cards')->insert([
+            'id' => "6",
+        'attack' => "Alors, ça baise sa main ?",
+        'category_id' => "2",
+    ]);
+    
+    DB::table('attack_cards')->insert([
+            'id' => "7",
+        'attack' => "Mini bite",
+        'category_id' => "4",
+    ]);
+    
+    DB::table('attack_cards')->insert([
+            'id' => "8",
+        'attack' => "Tu peux me régler mon imprimante stp ?",
+        'category_id' => "5",
+    ]);
+    
+    DB::table('attack_cards')->insert([
+            'id' => "9",
+        'attack' => "Retourne faire des maths le bigleux",
+        'category_id' => "6",
+    ]);
+    
+    DB::table('attack_cards')->insert([
+            'id' => "10",
+        'attack' => "Tu n'es pas beau",
+        'category_id' => "4",
+    ]);
+    
+    DB::table('attack_cards')->insert([
+            'id' => "11",
+        'attack' => "À 30 ans tu seras toujours puceau",
+        'category_id' => "2",
+    ]);
+    
+            
+    DB::table('attack_cards')->insert([
+            'id' => "12",
+        'attack' => "L'avancement de ta calvitie ?",
+        'category_id' => "4",
+    ]);
+    
+
+    DB::table('attack_cards')->insert([
+            'id' => "13",
+        'attack' => "T'es moche comme un cul",
+        'category_id' => "4",
+    ]);
+    
+    
+    DB::table('attack_cards')->insert([
+            'id' => "14",
+        'attack' => "Retourne te branler sur du hentai",
+        'category_id' => "2",
+    ]);
+    
+
+    DB::table('attack_cards')->insert([
+            'id' => "15",
+        'attack' => "Retourne dans les jupons de ta mère",
+        'category_id' => "3",
+    ]);
+    
+    
+    DB::table('attack_cards')->insert([
+            'id' => "16",
+        'attack' => "Fils à sa maman",
+        'category_id' => "3",
+    ]);
+
+    DB::table('attack_cards')->insert([
+            'id' => "17",
+        'attack' => "Laisse moi deviner, t'as encore raté ta tentative de suicide hier?",
+        'category_id' => "5",
+    ]);
+    DB::table('attack_cards')->insert([
+            'id' => "18",
+        'attack' => "Même ta mère n'a plus le courage de t'embrasser.",
+        'category_id' => "4",
+    ]);
+         
+    DB::table('attack_cards')->insert([
+            'id' => "19",
+        'attack' => "T'es plus moche que ce que j'ai lâché aux chiottes ce matin",
+        'category_id' => "4",
+    ]);
+
+    DB::table('attack_cards')->insert([
+            'id' => "20",
+        'attack' => "Qu'est-ce que tu fais hors de ton terrier?",
+        'category_id' => "5",
+    ]);
+    
+    DB::table('attack_cards')->insert([
+            'id' => "21",
+        'attack' => "Ça sentait bon avant que t'ouvres ta gueule.",
+        'category_id' => "1",
+    ]);
+    
+    DB::table('attack_cards')->insert([
+            'id' => "22",
+        'attack' => "À part ton chat qui te laisse le toucher ?",
+        'category_id' => "2",
+    ]);
+
+    DB::table('attack_cards')->insert([
+            'id' => "23",
+        'attack' => "Elle date de quand ta dernière douche ?",
+        'category_id' => "1",
+    ]);
+    DB::table('attack_cards')->insert([
+            'id' => "24",
+        'attack' => "Est-ce qu'il y a un rat mort dans le coin ? Ah non c'est juste toi...",
+        'category_id' => "1",
+    ]);
+    
+    DB::table('attack_cards')->insert([
+            'id' => "25",
+        'attack' => "T'as bouffé une poubelle au ptit dej ?",
+        'category_id' => "1",
+    ]);
+
+    DB::table('attack_cards')->insert([
+            'id' => "26",
+        'attack' => "Remet tes lunettes stp. Merci.",
+        'category_id' => "4",
+    ]);
+    
+    
+    DB::table('attack_cards')->insert([
+            'id' => "27",
+        'attack' => "Non non laisse ton masque c'est très bien comme ça.",
+        'category_id' => "4",
+    ]);
+
+    DB::table('attack_cards')->insert([
+            'id' => "28",
+        'attack' => "Tu sais t'es pas obligé de me parler.",
+        'category_id' => "5",
+    ]);
+    
+    
+    DB::table('attack_cards')->insert([
+            'id' => "29",
+        'attack' => "J'ai un copain",
+        'category_id' => "5",
+    ]);
+
+    DB::table('attack_cards')->insert([
+        'id' => "30",
+        'attack' => "Je suis lesbienne.",
+        'category_id' => "5",
+    ]);
+    
+    DB::table('attack_cards')->insert([
+            'id' => "31",
+        'attack' => "Excuse moi mais je vais aller me laver les yeux après avoir vu ta sale gueule de cul",
+        'category_id' => "4",
+    ]);
+      
+    DB::table('attack_cards')->insert([
+            'id' => "32",
+        'attack' => "Tu sais me prêter 30fr pour inviter mon copain au resto?",
+        'category_id' => "5",
+    ]);
+
+    DB::table('attack_cards')->insert([
+            'id' => "33",
+        'attack' => "T'as fait quoi à part jouer à Minecraft ce week-end ?",
+        'category_id' => "3",
+    ]);
+      
+    DB::table('attack_cards')->insert([
+            'id' => "34",
+        'attack' => "Simp",
+        'category_id' => "5",
+    ]);
+
+    DB::table('attack_cards')->insert([
+            'id' => "35",
+        'attack' => "Cuck",
+        'category_id' => "5",
+    ]);
+    
+    DB::table('attack_cards')->insert([
+            'id' => "36",
+        'attack' => "Tu sais depanner mon copain? Son pc démarre plus.",
+        'category_id' => "5",
+    ]);
+    
+             DB::table('attack_cards')->insert([
+            'id' => "37",
+        'attack' => "Combien t'as donné à ta streameuse préférée ce mois-ci vieux cuck ?",
+        'category_id' => "5",
+    ]);
+    
+    
     }
 }
--- database/seeders/RipostCardSeeder.php
+++ database/seeders/RipostCardSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RipostCardSeeder extends Seeder
 {
@@ -13,6 +14,195 @@ class RipostCardSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('ripost_cards')->insert([
+            'id' => "1",
+        'ripost' => "Si si, juste ta mère",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "2",
+        'ripost' => "Et toi la bite",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "3",
+        'ripost' => "Sandwich au sperme, comme d'habitude ?",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "4",
+        'ripost' => "Si si, ya juste ta mère dedans",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "5",
+        'ripost' => "Et toi ton as une grosse entre les deux épaules",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "6",
+        'ripost' => "Ma main je la met où je veux, et c'est soit sur ma queue, soit dans ta gueule !",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "7",
+        'ripost' => "Mini bite, grosses couilles",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "8",
+        'ripost' => "Ok mais d'abord je règle ta mère",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "9",
+        'ripost' => "Je te calcule même pas",   /// à modifier si mieux
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "10",
+        'ripost' => "",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "11",
+        'ripost' => "Et toi à 30 ans tu seras toujours à l'école",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "12",
+        'ripost' => "Au point mort, comme ta vie",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "13",
+        'ripost' => "Si c'est le tiens, je me plains pas",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "14",
+        'ripost' => "It's called 'Hentai', and it's art.",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "15",
+        'ripost' => "Pas le temps, je suis perdu dans ceux de la tienne",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "16",
+        'ripost' => "Au moins la mienne est encore en vie",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "17",
+        'ripost' => "Malheureusement oui, ça m'aurait évité de voir ta gueule",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "18",
+        'ripost' => "Par contre la tienne elle n'y vas pas de main morte",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "19",
+        'ripost' => "T'as vu ta réflection au fonds de la cuvette ?",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "20",
+        'ripost' => "",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "21",
+        'ripost' => "",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "22",
+        'ripost' => "La liste est assez longue, ta soeur, ta grand mère, ta tante, je continue ?",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "23",
+        'ripost' => "Bah ce matin. et Toc !",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "24",
+        'ripost' => "Est-ce qu'il y un .... dans le coin ? Ah non c'est juste toi...",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "25",
+        'ripost' => "Non, je me suis fait un petit muesli avec un ovomaltine des familles",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "26",
+        'ripost' => "Non ça va aller, ça m'évite de voir ta gueule",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "27",
+        'ripost' => "T'as bien raison, ça m'évitera de chopper une de tes merdes",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "28",
+        'ripost' => "Tu me faisais pitié, je me suis senti obligé",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "29",
+        'ripost' => "J'apprécie l'offre mais tu ne rentres pas dans mes standards, NEXT",
+    ]);
+    
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "30",
+        'ripost' => "C'est pas en changeant d'équipe que t'augmente tes chances de gagner",
+    ]);
+    
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "31",
+        'ripost' => "Tu peux aussi laver le reste par la même occasion",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "32",
+        'ripost' => "Désolé mais il mange déjà avec moi ce soir",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "33",
+        'ripost' => "J'ai lu le Suicide français d'Éric Zemmour, et toi?",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "34",
+        'ripost' => "Cuck",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "35",
+        'ripost' => "Simp",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "36",
+        'ripost' => "Normal, je l'ai hacké ce weekend",
+    ]);
+    
+    DB::table('ripost_cards')->insert([
+            'id' => "37",
+        'ripost' => "Autant que t'as gagné sur OnlyFans",
+    ]);
+    
+    
+    
+    
     }
 }
